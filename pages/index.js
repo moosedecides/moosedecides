5b3e66f7cad1d64bafea9db29af9e8f22bf511e2
easy scene <easyscene@easys-Mac-mini.local>
2026-03-21 19:41:21 -0400

    Detailed moose head logo for favicon and link previews

--- pages/index.js
+++ pages/index.js
@@ -46,7 +46,6 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
         {/* Favicon */}
-        <link rel="icon" type="image/svg+xml" href="/moose-head.svg" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16.png" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
