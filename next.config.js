e48a0561b8709149af803aeba43fa8f4616b6676
easy scene <easyscene@mac.mynetworksettings.com>
2026-03-22 03:37:22 -0400

    Use serpapi_thumbnail for product images (proxied, no CORS issues)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'encrypted-tbn0.gstatic.com' },
+      { protocol: 'https', hostname: 'encrypted-tbn1.gstatic.com' },
+      { protocol: 'https', hostname: 'encrypted-tbn2.gstatic.com' },
+      { protocol: 'https', hostname: 'encrypted-tbn3.gstatic.com' },
+      { protocol: 'https', hostname: 'serpapi.com' },
+    ],
+  },
 };
 
 module.exports = nextConfig;
